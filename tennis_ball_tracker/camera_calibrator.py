44cafd2e6a74a1e31142f8872a71491ccecb7c26
Quinten Simet <quinten@mastodondesign.com>
2021-04-28 13:53:29 -0400

    finished the camera calibrator
    this will allow us to generate calibration images to use
    to calibrate our camera

--- tennis_ball_tracker/camera_calibrator.py
+++ tennis_ball_tracker/camera_calibrator.py
@@ -1,5 +1,6 @@
 # Standard library imports
 import os
+import glob
 import uuid
 import time
 import tempfile
@@ -15,6 +16,7 @@ from tennis_ball_tracker.CameraFeed import CameraFeed
 
 message_queue = Queue(-1)
 lock = threading.Event()
+lock.set()
 
 class CameraCalibrator(threading.Thread):
     """
@@ -35,7 +37,9 @@ class CameraCalibrator(threading.Thread):
 
     def __init__(self, grid_pattern, path_to_store_imgs = tempfile.gettempdir(), img_ext = ".png", use_circle_grid = False,  camera = None):
         super(CameraCalibrator, self).__init__(name="Camera Calibrator")
-        self.path_to_store_imgs = path_to_store_imgs
+        self.LEFT_PATH = os.path.join(path_to_store_imgs, "left")
+        self.RIGHT_PATH = os.path.join(path_to_store_imgs, "right")
+
         self.grid_pattern = grid_pattern
         self.img_ext = img_ext
         self.use_circle_grid = use_circle_grid
@@ -66,7 +70,8 @@ class CameraCalibrator(threading.Thread):
             img_ext (str): The image extention to use for the calibration images.
             img (): The image to store.
         """
-        file_ = os.path.join(path_to_store_imgs, str(uuid.uuid3) + img_ext)
+        file_ = os.path.join(path_to_store_imgs, "cal-" + str(uuid.uuid1()) + img_ext)
+        message_queue.put("Saving captured calibration image " + file_ + "...")
         self.camera.saveStereoFrame(file_, img)
 
     def remove_stale_calibration_files(self, path_to_store_imgs):
@@ -75,50 +80,64 @@ class CameraCalibrator(threading.Thread):
 
         Args:
             path_to_store_imgs (str): Path where calibration files may be stored.
+        """        
+        message_queue.put("Clearing any sale calibration files in " + path_to_store_imgs + "...")
+        if os.path.isdir(path_to_store_imgs):
+            images = glob.glob(os.path.join(path_to_store_imgs, "*"))
+            [os.remove(image) for image in images]
+            os.rmdir(path_to_store_imgs)
+
+    def setup_calibration_folders(self, path_to_store_imgs):
         """
-        LEFT_PATH = os.path.join(path_to_store_imgs, "left")
-        RIGHT_PATH = os.path.join(path_to_store_imgs, "right")
+        Create calibration folders.
         
-        if os.path.isdir(LEFT_PATH):
-            os.rmdir(LEFT_PATH)
-        if os.path.isdir(RIGHT_PATH):
-            os.rmdir(RIGHT_PATH)
+        These folders will hold the camera calibration files.
+
+        Args:
+            path_to_store_imgs (str): Path where calibration files may be stored.
+        """
+        message_queue.put("Creating the calibration folder " + path_to_store_imgs + "...")
+        if not os.path.isdir(path_to_store_imgs):
+            os.mkdir(path_to_store_imgs)
 
     def run(self):
         """Run the camera calibration process."""
         message_queue.put("Starting the Camera Calibration process...")
 
-        LEFT_PATH = os.path.join(self.path_to_store_imgs, "left")
-        RIGHT_PATH = os.path.join(self.path_to_store_imgs, "right")
-
-        message_queue.put("Clearing any sale calibration files...")
-
-        self.remove_stale_calibration_files(self.path_to_store_imgs)
+        self.remove_stale_calibration_files(self.LEFT_PATH)
+        self.remove_stale_calibration_files(self.RIGHT_PATH)
+        self.setup_calibration_folders(self.LEFT_PATH)
+        self.setup_calibration_folders(self.RIGHT_PATH)
 
         valid_imgs = 0
         while (valid_imgs != self.REQUIRED_NUMBER_OF_IMGS):
             message_queue.put("Attempting to acquire images {} out of {}...".format(valid_imgs+1, self.REQUIRED_NUMBER_OF_IMGS))
-            left, right = self.camera.getStereoFrames()
+            left, right = self.camera.getStereoRGB()
+            left_greyscale = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
+            right_greyscale = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
 
             message_queue.put("Validating images {} out of {}...".format(valid_imgs+1, self.REQUIRED_NUMBER_OF_IMGS))
-            if (self.verify_pattern(left.GREYSCALE, self.grid_pattern) and self.verify_pattern(right.GREYSCALE, self.grid_pattern)):
+            if (self.verify_pattern(left_greyscale, self.grid_pattern) and self.verify_pattern(right_greyscale, self.grid_pattern)):
                 message_queue.put("Images {} out of {} are valid!".format(valid_imgs+1, self.REQUIRED_NUMBER_OF_IMGS))
-                message_queue.put("Saving the captured calibration images {} out of {}...".format(valid_imgs+1, self.REQUIRED_NUMBER_OF_IMGS))
-                self.save_calibration_image(LEFT_PATH, self.img_ext, left.RGB)
-                self.save_calibration_image(RIGHT_PATH, self.img_ext, right.RGB)
+                self.save_calibration_image(self.LEFT_PATH, self.img_ext, left)
+                self.save_calibration_image(self.RIGHT_PATH, self.img_ext, right)
                 valid_imgs+= 1
             else:
                 message_queue.put("Images {} out of {} are invalid!".format(valid_imgs+1, self.REQUIRED_NUMBER_OF_IMGS))
-            message_queue.put("Please the checkerboard in preporation of our next calibration image...")
-            time.sleep(5)
-            # lock.wait()
+            message_queue.put("Please move the checkerboard in preporation of our next calibration image...")
+            lock.clear()
+            lock.wait()
 
         message_queue.put("Camera Calibration process complete!")
 
 if __name__ == "__main__":
-    calibrator = CameraCalibrator((7,7))
+    calibrator = CameraCalibrator((8, 6))
     calibrator.start()
     while calibrator.is_alive():
-        msg = message_queue.get(True)
-        print(msg)
+        if not message_queue.empty():
+            msg = message_queue.get(True)
+            print(msg)
+        if not lock.is_set():
+            raw_input("Press enter to capture the next calibration img")
+            lock.set()
     calibrator.join()
